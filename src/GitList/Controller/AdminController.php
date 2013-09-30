cf9d74273bdfe022f50357c2fbb8d369fdbfa6ad
unknown <froth@.united.domain>
2013-09-30 11:23:52 +0200

    Security fix, PHP-Session changed to symphony session.

--- src/GitList/Controller/AdminController.php
+++ src/GitList/Controller/AdminController.php
@@ -8,148 +8,141 @@ use Silex\ControllerProviderInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class AdminController implements ControllerProviderInterface {
 
 	public function connect(Application $app) {
 		$route = $app['controllers_factory'];
-		
-		session_start();
-		
-		# Logout-Request
-		$route -> get('/admin/logout', function() use ($app) {
-			session_unset();
+
+		# start session
+		$session = new Session();
+		$session -> start();
+
+		# Logout
+		$route -> get('/admin/logout', function() use ($app, $session) {
+			$session -> invalidate();
 			return $app['twig'] -> render('admin_login.twig', array());
-		}) -> bind('logout');		
-		
-		
-		# Loginformular
-		$route -> get('/admin/login', function() use ($app) {
-			if(isset($_SESSION['admin'])){
-				return $this->adminpanel($app);								
+		}) -> bind('logout');
+
+		# Login
+		$route -> get('/admin/login', function() use ($app, $session) {
+			if ($session -> has('admin')) {
+				return $this -> adminpanel($app, $session);
 			} else {
 				return $app['twig'] -> render('admin_login.twig', array());
 			}
 		}) -> bind('login');
-		
-		
-		# Login-Request
-		$route -> post('/admin/login', function(Request $request) use ($app) {
+
+		# Login (form submit)
+		$route -> post('/admin/login', function(Request $request) use ($app, $session) {
 			$name = $request -> get('gitlist_name');
 			$password = $request -> get('gitlist_password');
 
 			# check login data
-			if($app['admin.name'] == $name && $app['admin.password'] == $password){
+			if ($app['admin.name'] == $name && $app['admin.password'] == $password) {
 				# log admin in
-				$_SESSION['admin'] = $app['admin.name'];
-				
+				$session -> set('admin', $app['admin.name']);
+
 				# goto adminpanel
-				return $this->adminpanel($app);
-							
+				return $this -> adminpanel($app, $session);
+
 			} else {
-				return $app['twig'] -> render('admin_login.twig', array(
-					'error' => 'Wrong login! Try again.'
-				));
+				return $app['twig'] -> render('admin_login.twig', array('error' => 'Wrong login! Try again.'));
 			}
 		});
-		
-		
+
 		# Adminpanel
-		$route -> get('/admin/admin', function() use ($app) {
-			return $this->adminpanel($app);
+		$route -> get('/admin/admin', function() use ($app, $session) {
+			return $this -> adminpanel($app, $session);
 		}) -> bind('adminpanel');
 
+		# AJAX: Adminpanel: Delete a repositories path out of the configuration
+		$route -> get('/admin/admin/delete/{repospathBase64}', function($repospathBase64) use ($app, $session) {
+			if ($session -> has('admin')) {
 
-		# Adminpanel: Delete a repositories path out of the configuration (ajax)
-		$route -> get('/admin/admin/delete/{repospathBase64}', function($repospathBase64) use ($app) {
+				$config = GitList\Config::fromFile($app -> getPath() . 'config.ini');
 
-			$config = GitList\Config::fromFile($app->getPath() .'config.ini');
+				$repospathToDelete = base64_decode($repospathBase64);
+				$repospaths = $app['git.repos'];
 
-			$repospathToDelete = base64_decode($repospathBase64);
-			$repospaths = $app['git.repos'];
+				# delete it out!
+				$index = array_search($repospathToDelete, $repospaths);
+				if ($index !== FALSE) {
+					unset($repospaths[$index]);
+				} else {
+					return new Response(json_encode(array('type' => 'error', 'message' => 'Can\'t delete! Repositories list <strong>' . $repospathToDelete . '<strong> not found.')));
+				}
 
-			# delete it out!
-			$index = array_search($repospathToDelete, $repospaths);
-			if ($index !== FALSE) {
-				unset($repospaths[$index]);
+				# keep debug and cache in ini, php-ini false proptery is the same like an empty property
+				$config -> set('app', 'cache', (empty($data['app']['cache']) || $data['app']['cache'] == 'false') ? 'false' : 'true');
+				$config -> set('app', 'debug', (empty($data['app']['debug']) || $data['app']['debug'] == 'false') ? 'false' : 'true');
+
+				# save to config.ini
+				$config -> set('git', 'repositories', $repospaths);
+				$config -> toFile($app -> getPath() . 'config.ini');
+
+				return new Response(json_encode(array('type' => 'success', 'message' => 'Repositories list <strong>' . $repospathToDelete . '</strong> was deleted successfully from your GitList.')), 200);
 			} else {
-				return new Response(json_encode(array('type' => 'error', 'message' => 'Can\'t delete! Repositories list <strong>'.$repospathToDelete.'<strong> not found.')));				
+				return new Response(json_encode(array('type' => 'error', 'message' => 'You are not logged in!')));
 			}
-			
-			# keep debug and cache in ini, php-ini false proptery is the same like an empty property
-			$config -> set('app', 'cache', (empty($data['app']['cache']) || $data['app']['cache'] == 'false') ? 'false' : 'true');
-			$config -> set('app', 'debug', (empty($data['app']['debug'])  || $data['app']['debug'] == 'false') ? 'false' : 'true');
-			 
-			# save to config.ini
-			$config -> set('git', 'repositories', $repospaths);
-			$config -> toFile($app->getPath() .'config.ini');
-			
-			return new Response(json_encode(array(
-				'type' => 'success',
-				'message' => 'Repositories list <strong>' . $repospathToDelete . '</strong> was deleted successfully from your GitList.'
-			)), 200);
 		});
 
 		# Adminpanel: Change configuration
-		$route -> post('/admin/admin', function(Request $request) use ($app) {
-			
-			# read in current config
-			$config = GitList\Config::fromFile($app->getPath() .'config.ini');
+		$route -> post('/admin/admin', function(Request $request) use ($app, $session) {
+			if ($session -> has('admin')) {
+				# read in current config
+				$config = GitList\Config::fromFile($app -> getPath() . 'config.ini');
 
-			# transform to true or false
-			$debug = $request -> get('debug') == 'yes' ? 'true' : 'false';
-			$cache = $request -> get('cache') == 'yes' ? 'true' : 'false';
+				# transform to true or false
+				$debug = $request -> get('debug') == 'yes' ? 'true' : 'false';
+				$cache = $request -> get('cache') == 'yes' ? 'true' : 'false';
 
-			# create new config
-			$data = array();
-			
-			# add abostrohpe if it is a windows system
-			if($this->isWindows()){
-				$data['git']['client'] = "\"".$request -> get('gitpath')."\"";
-			} else {
-				$data['git']['client'] = $request -> get('gitpath');
-			}
-			
-			$data['git']['default_branch'] = $request -> get('defaultbranch');
-			$data['git']['repositories'] = $app['git.repos'];
-			$data['app']['debug'] = $debug;
-			$data['app']['cache'] = $cache;
+				# create new config
+				$data = array();
 
-			# add new repository path
-			
-			# check if repository exists
-			$newpath = $request -> get('newpath');
-			
-			$notification = 'saved';
-				
-			if (!empty($newpath)) {
-				try {
-					$options['path'] = $app['git.client'];
-		            $options['hidden'] = $app['git.hidden'];
-		            $options['ini.file'] = $app['ini.file'];
-		            $options['default_branch'] = $app['git.default_branch'];
-		            $client = new GitList\Git\Client($options);
-					$client->getRepositories(array($newpath));		
-
-					# if repository path has a git repos, add new repositories path to other paths
-					array_push($data['git']['repositories'], $newpath);
-				} catch (\RuntimeException $e) {
-				    $notification = 'error_wrong_path';
+				# add abostrohpe if it is a windows system
+				if ($this -> isWindows()) {
+					$data['git']['client'] = "\"" . $request -> get('gitpath') . "\"";
+				} else {
+					$data['git']['client'] = $request -> get('gitpath');
 				}
-			}
-			
-			$config -> addAndOverwrite($data);
-			$config -> toFile($app->getPath() .'config.ini');
 
-			return $app['twig'] -> render('admin.twig', array(
-				'repositorypaths' => $this->repositoriesAddBase64Attribute($data['git']['repositories']), 
-				'client' => trim($data['git']['client'], '"'),
-				'default_branch' => $data['git']['default_branch'], 
-				'cache' => (empty($data['app']['cache']) || $data['app']['cache'] == 'false') ? false : true, 
-				'debug' => (empty($data['app']['debug'])  || $data['app']['debug'] == 'false') ? false : true,
-				'notification' => $notification,
-				'newpath' => $newpath
-			));
+				$data['git']['default_branch'] = $request -> get('defaultbranch');
+				$data['git']['repositories'] = $app['git.repos'];
+				$data['app']['debug'] = $debug;
+				$data['app']['cache'] = $cache;
+
+				# add new repository path
+
+				# check if repository exists
+				$newpath = $request -> get('newpath');
+
+				$notification = 'saved';
+
+				if (!empty($newpath)) {
+					try {
+						$options['path'] = $app['git.client'];
+						$options['hidden'] = $app['git.hidden'];
+						$options['ini.file'] = $app['ini.file'];
+						$options['default_branch'] = $app['git.default_branch'];
+						$client = new GitList\Git\Client($options);
+						$client -> getRepositories(array($newpath));
+
+						# if repository path has a git repos, add new repositories path to other paths
+						array_push($data['git']['repositories'], $newpath);
+					} catch (\RuntimeException $e) {
+						$notification = 'error_wrong_path';
+					}
+				}
+
+				$config -> addAndOverwrite($data);
+				$config -> toFile($app -> getPath() . 'config.ini');
+
+				return $app['twig'] -> render('admin.twig', array('repositorypaths' => $this -> repositoriesAddBase64Attribute($data['git']['repositories']), 'client' => trim($data['git']['client'], '"'), 'default_branch' => $data['git']['default_branch'], 'cache' => (empty($data['app']['cache']) || $data['app']['cache'] == 'false') ? false : true, 'debug' => (empty($data['app']['debug']) || $data['app']['debug'] == 'false') ? false : true, 'notification' => $notification, 'newpath' => $newpath));
+			} else {
+				return $app['twig'] -> render('admin_login.twig', array());
+			}
 		}) -> bind('savetoconfig');
 
 		return $route;
@@ -164,33 +157,24 @@ class AdminController implements ControllerProviderInterface {
 		}
 		return $repositories_with_base64;
 	}
-	
-	
-	private function adminpanel($app){
-		if(isset($_SESSION['admin'])){
-			return $app['twig'] -> render('admin.twig', array(
-				'repositorypaths' => $this -> repositoriesAddBase64Attribute($app['git.repos']), 
-				'client' => trim($app['git.client'], '"'), 
-				'default_branch' => $app['git.default_branch'], 
-				'cache' => (empty($app['twig.options']['cache']) || $app['twig.options']['cache'] == 'false') ? false : true, 
-				'debug' => (empty($app['debug'])  || $app['debug'] == 'false') ? false : true,
-				'admin' => $_SESSION['admin']
-			));
+
+	private function adminpanel($app, $session) {
+		if ($session -> has('admin')) {
+			return $app['twig'] -> render('admin.twig', array('repositorypaths' => $this -> repositoriesAddBase64Attribute($app['git.repos']), 'client' => trim($app['git.client'], '"'), 'default_branch' => $app['git.default_branch'], 'cache' => (empty($app['twig.options']['cache']) || $app['twig.options']['cache'] == 'false') ? false : true, 'debug' => (empty($app['debug']) || $app['debug'] == 'false') ? false : true, 'admin' => $session -> get('admin')));
 		} else {
 			return $app['twig'] -> render('admin_login.twig', array());
-		}		
-	}	
-	
-	private function isWindows()
-    {
-        switch (PHP_OS) {
-            case 'WIN32':
-            case 'WINNT':
-            case 'Windows':
-                return true;
-            default:
-                return false;
-        }
-    }
+		}
+	}
+
+	private function isWindows() {
+		switch (PHP_OS) {
+			case 'WIN32' :
+			case 'WINNT' :
+			case 'Windows' :
+				return true;
+			default :
+				return false;
+		}
+	}
 
 }
