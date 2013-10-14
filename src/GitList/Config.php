609bb054ac5531e092b95c89cd916b7780e53b71
unknown <froth@.united.domain>
2013-10-14 11:23:54 +0200

    Adminpanel remake

--- src/GitList/Config.php
+++ src/GitList/Config.php
@@ -2,138 +2,143 @@
 
 namespace GitList;
 
-class Config
-{
-    protected $data;
+class Config {
+	protected $data;
 
-    public static function fromFile($file)
-    {
-        if (!file_exists($file)) {
-            die(sprintf('Please, create the %1$s file.', $file));
-        }
-
-        $data = parse_ini_file($file, true);
-        $config = new static($data);
-        $config->validateOptions();
-
-        return $config;
-    }
-
-    public function __construct($data = array())
-    {
-        $this->data = $data;
-    }
-
-    public function get($section, $option)
-    {
-        if (!array_key_exists($section, $this->data)) {
-            return false;
-        }
-
-        if (!array_key_exists($option, $this->data[$section])) {
-            return false;
-        }
-
-        return $this->data[$section][$option];
-    }
-
-    public function getSection($section)
-    {
-        if (!array_key_exists($section, $this->data)) {
-            return false;
-        }
-
-        return $this->data[$section];
-    }
-
-    public function set($section, $option, $value)
-    {
-        $this->data[$section][$option] = $value;
-    }
-
-    protected function validateOptions()
-    {
-        $repositories = $this->get('git', 'repositories');
-
-        $atLeastOneOk = false;
-        $atLeastOneWrong = false;
-
-		if(!empty($repositories)){
-	        foreach ($repositories as $directory) {
-	            if (!$directory || !is_dir($directory)) {
-	                $atLeastOneWrong = true;
-	            } else {
-	                $atLeastOneOk = true;
-	            }
-	        }
+	public static function fromFile($file) {
+		if (!file_exists($file)) {
+			die(sprintf('Please, create the %1$s file.', $file));
 		}
 
-        if (!$atLeastOneOk) {
-            die("Please, edit the config file and provide your repositories directory");
-        }
+		$data = parse_ini_file($file, true);
 
-        if ($atLeastOneWrong) {
-            die("One or more of the supplied repository paths appears to be wrong. Please, check the config file");
-        }
-    }
-	
-	public function toFile($file)
-	{
-		$this->write_ini_file($this->data, $file);
+		// If attribute in property file is false, then it is interpreted as unset
+		// So on next save operation the property is lost
+		if (empty($data['app']['debug'])) {
+			$data['app']['debug'] = 'false';
+		} else {
+			$data['app']['debug'] = 'true';
+		}
+		if (empty($data['app']['cache'])) {
+			$data['app']['cache'] = 'false';
+		} else {
+			$data['app']['cache'] = 'true';
+		}
+		if (empty($data['app']['public'])) {
+			$data['app']['public'] = 'false';
+		} else {
+			$data['app']['public'] = 'true';
+		}
+
+		$config = new static($data);
+		$config -> validateOptions();
+
+		return $config;
 	}
-	
-	private function write_ini_file($assoc_arr, $path) 
-	{ 
-	    $content = ""; 
 
-        foreach ($assoc_arr as $key=>$elem) { 
-            $content .= "[".$key."]\n"; 
-            foreach ($elem as $key2=>$elem2) { 
-                if(is_array($elem2)) 
-                { 
-                    for($i=0;$i<count($elem2);$i++) 
-                    {
-                    	if(isset($elem2[$i])){
-	                    	# write true or false as keywords without absostrophe
-	                    	if($elem2[$i] == 'true' || $elem2[$i] == 'false'){
-								$content .= $key2."[] = ".$elem2[$i]."\n"; 
-							}  else {
-								$content .= $key2."[] = '".$elem2[$i]."'\n"; 
-							}						
-                    	}
-                    } 
-                } 
-                else if($elem2==""){
-                	$content .= $key2." = \n";
-                }  
-                else{
-                	# write true or false as keywords without absostrophe
-					if($elem2 == 'true' || $elem2 == 'false'){
-						$content .= $key2." = ".$elem2."\n";
-					}  else {
-						$content .= $key2." = '".$elem2."'\n";
+	public function __construct($data = array()) {
+		$this -> data = $data;
+	}
+
+	public function get($section, $option) {
+		if (!array_key_exists($section, $this -> data)) {
+			return false;
+		}
+
+		if (!array_key_exists($option, $this -> data[$section])) {
+			return false;
+		}
+
+		return $this -> data[$section][$option];
+	}
+
+	public function getSection($section) {
+		if (!array_key_exists($section, $this -> data)) {
+			return false;
+		}
+
+		return $this -> data[$section];
+	}
+
+	public function set($section, $option, $value) {
+		$this -> data[$section][$option] = $value;
+	}
+
+	protected function validateOptions() {
+		$repositories = $this -> get('git', 'repositories');
+
+		$atLeastOneOk = false;
+		$atLeastOneWrong = false;
+
+		if (!empty($repositories)) {
+			foreach ($repositories as $directory) {
+				if (!$directory || !is_dir($directory)) {
+					$atLeastOneWrong = true;
+				} else {
+					$atLeastOneOk = true;
+				}
+			}
+		}
+
+		if (!$atLeastOneOk) {
+			die("Please, edit the config file and provide your repositories directory");
+		}
+
+		if ($atLeastOneWrong) {
+			die("One or more of the supplied repository paths appears to be wrong. Please, check the config file");
+		}
+	}
+
+	public function toFile($file) {
+		$this -> write_ini_file($this -> data, $file);
+	}
+
+	private function write_ini_file($assoc_arr, $path) {
+		$content = "";
+
+		foreach ($assoc_arr as $key => $elem) {
+			$content .= "[" . $key . "]\n";
+			foreach ($elem as $key2 => $elem2) {
+				if (is_array($elem2)) {
+					for ($i = 0; $i < count($elem2); $i++) {
+						if (isset($elem2[$i])) {
+							# write true or false as keywords without absostrophe
+							if ($elem2[$i] == 'true' || $elem2[$i] == 'false') {
+								$content .= $key2 . "[] = " . $elem2[$i] . "\n";
+							} else {
+								$content .= $key2 . "[] = '" . $elem2[$i] . "'\n";
+							}
+						}
 					}
-				} 
-            } 
-        }
-	  
-	
-	    if (!$handle = fopen($path, 'w')) { 
-	        return false; 
-	    } 
-	    if (!fwrite($handle, $content)) { 
-	        return false; 
-	    } 
-	    fclose($handle); 
-	    return true; 
-	}	
+				} else if ($elem2 == "") {
+					$content .= $key2 . " = \n";
+				} else {
+					# write true or false as keywords without absostrophe
+					if ($elem2 == 'true' || $elem2 == 'false') {
+						$content .= $key2 . " = " . $elem2 . "\n";
+					} else {
+						$content .= $key2 . " = '" . $elem2 . "'\n";
+					}
+				}
+			}
+		}
 
-	public function getData(){
-		return $this->data;
+		if (!$handle = fopen($path, 'w')) {
+			return false;
+		}
+		if (!fwrite($handle, $content)) {
+			return false;
+		}
+		fclose($handle);
+		return true;
 	}
-	
-	public function addAndOverwrite($data_array){
-		$this->data = array_merge($this->data, $data_array);
+
+	public function getData() {
+		return $this -> data;
 	}
+
+	public function addAndOverwrite($data_array) {
+		$this -> data = array_merge($this -> data, $data_array);
+	}
+
 }
-
--- web/js/admin.js
+++ web/js/admin.js
@@ -1,39 +1,182 @@
-$(function() {
+jQuery(document).ready(function($) {
 
-	function error(message) {
-		$("div.messages").html('<div class="alert alert-error"><a class="close" data-dismiss="alert">×</a>' + message + '</div>');
+	function error(message, target) {
+		$(target).html('<div class="alert alert-error"><a class="close" data-dismiss="alert">×</a>' + message + '</div>');
 	}
 
-	function success(message) {
-		$("div.messages").html('<div class="alert alert-success"><a class="close" data-dismiss="alert">×</a>' + message + '</div>');
+	function success(message, target) {
+		$(target).html('<div class="alert alert-success"><a class="close" data-dismiss="alert">×</a>' + message + '</div>');
 	}
 
+	// Tabs
+	$('#adminTabs a').click(function(e) {
+		e.preventDefault();
+		var url = $(this).attr("data-url");
+		var pane = $(this);
+		pane.tab('show');
+	});
 
-	$(".remove-repo").on('click', function() {
-		var repofilderBase64 = $(this).attr('data-repofolder');
-		var $removeButton = $(this);
+	// load first tab content
+	$('#paths').load($('.active a').attr("data-url"), function(result) {
+		$('.active a').tab('show');
+	});
 
-		$.getJSON('admin/delete/' + repofilderBase64, function(response) {
-			if (response.type == 'success') {
+	// Change properties in config.ini
+	var timeout;
 
-				$removeButton.closest('tr').fadeOut(300, function() {
-					$removeButton.remove();
-					success(response.message);
+	// Simple properties like booleans and strings
+	$(".singleProperty").on('input change', function() {
+		clearTimeout(timeout);
+		var self = this;
+		timeout = setTimeout(function() {
+			$.ajax({
+				url : 'edit/' + $(self).attr('name'),
+				type : 'POST',
+				data : {
+					value : $(self).attr('value')
+				},
+				dataType : 'json',
+				success : function(data, textStatus, jqXHR) {
+					if (data.type == 'succeed') {
+						success(data.message, "div.messages");
+					} else {
+						error(data.message, "div.messages");
+					}
+				}
+			});
+		}, 200);
+	});
 
-				});
-			} else if (response.type == 'error') {
-				error(response.message);
+	// Add a new account
+	$("#add_account").on('click', function() {
+		var name = $("#add_account_name").val();
+		var password = $("#add_account_password").val();
+		var role = $("#add_account_role").val();
+		var attribute;
+
+		if (name == '') {
+			error('Field <strong>name</strong> can not be empty!', "div.add_account_modal_messages");
+			return false;
+		}
+
+		if (password == '') {
+			error('Field <strong>password</strong> can not be empty!', "div.add_account_modal_messages");
+			return false;
+		}
+
+		if (role == 'ROLE_ADMIN') {
+			attribute = 'admins';
+		} else if (role == 'ROLE_USER') {
+			attribute = 'users';
+		}
+
+		$.ajax({
+			url : 'add_account/' + attribute,
+			type : 'POST',
+			data : {
+				name : name,
+				password : password
+			},
+			dataType : 'json',
+			success : function(data, textStatus, jqXHR) {
+				if (data.type == 'succeed') {
+					success(data.message, "div.messages");
+					$("#add_account_modal").modal("hide");
+					$("div.add_account_modal_messages").hide();
+					$("#add_account_name").val('');
+					$("#add_account_password").val('');
+
+					var icon;
+
+					if (role == 'ROLE_ADMIN') {
+						icon = 'icon-wrench';
+					} else if (role == 'ROLE_USER') {
+						icon = 'icon-user';
+					}
+					$("#accounts_table").append('<tr>' + '<td><i class="' + icon + ' icon-spaced"></i>' + name + '</td>' + '<td>' + role + '</td>' + '<td><a class="btn remove-account" href="#" data-name="' + name + '">remove</a></td>' + '</tr>');
+				} else {
+					error(data.message, "div.add_account_modal_messages");
+				}
 			}
 		});
-
 		return false;
 	});
 
-	$('#public_true:radio').change(function() {
-		$("#login_data").fadeOut();		
+	// Remove account
+	$(document).on('click', '.remove-account', function() {
+		var name = $(this).attr('data-name');
+		var self = $(this);
+		$.ajax({
+			url : 'remove_account/' + name,
+			type : 'GET',
+			dataType : 'json',
+			success : function(data, textStatus, jqXHR) {
+				if (data.type == 'succeed') {
+					success(data.message, "div.messages");
+					self.closest('tr').fadeOut(300, function() {
+						$(this).remove();
+					});
+				} else {
+					error(data.message, "div.messages");
+				}
+			}
+		});
+
 	});
-	
-	$('#public_false:radio').change(function() {
-		$("#login_data").fadeIn();
+
+	// Add repository
+	$("#add_repo").on('click', function() {
+		var repository = $("#add_repo_path").val();
+
+		$.ajax({
+			url : 'add_repo',
+			type : 'POST',
+			data : {
+				newrepopath : repository
+			},
+			dataType : 'json',
+			success : function(data, textStatus, jqXHR) {
+
+				if (data.type == 'succeed') {
+					success(data.message, "div.messages");
+					$("#add_repo_modal").modal("hide");
+					$("div.add_repo_modal_messages").hide();
+					$("#add_repo_path").val('');
+
+					$("#repos_table").append('<tr>' + '<td><i class="icon-folder-open icon-spaced"></i><a href="' + repository + '">' + repository + '</a></td>' + '<td><a class="btn remove-repo" href="#" data-repofolder="' + repository + '">remove</a></td>' + '</tr>');
+				} else {
+					error(data.message, "div.add_repo_modal_messages");
+				}
+			}
+		});
+		return false;
 	});
+
+	// Remove repository
+	$(document).on('click', '.remove-repo', function() {
+		var repofolder = $(this).attr('data-repofolder');
+		var self = $(this);
+
+		$.ajax({
+			url : 'remove_repo',
+			type : 'POST',
+			dataType : 'json',
+			data : {
+				repo : repofolder
+			},
+			success : function(data, textStatus, jqXHR) {
+				if (data.type == 'succeed') {
+					success(data.message, "div.messages");
+					self.closest('tr').fadeOut(300, function() {
+						$(this).remove();
+					});
+				} else {
+					error(data.message, "div.messages");
+				}
+			}
+		});
+		return false;
+	});
+
 });
+
